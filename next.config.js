b4cb9bfdbd7c432ed53b237b71e1670cc274ee54
ping1au <>
2023-10-03 23:04:06 +0800

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     env: {
-//         BASE_URL: process.env.BASE_URL,
-//       }
-// }
-
-// module.exports = nextConfig
-
-module.exports = async (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    /* config options here */
-  }
-  return nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
+ 
+module.exports = nextConfig
