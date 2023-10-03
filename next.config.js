9ce30020fb30adc70856b3bf7f45383313504bac
ping1au <>
2023-10-03 21:44:36 +0800

    Changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        BASE_URL: process.env.BASE_URL,
-      }
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     env: {
+//         BASE_URL: process.env.BASE_URL,
+//       }
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  }
+  return nextConfig
+}
