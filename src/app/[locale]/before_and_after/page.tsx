b6adfc671e2079bd9a217113da126041c854a48b
ping1au <>
2024-02-15 21:44:46 +0800

    fixed before & after

--- src/app/[locale]/before_and_after/page.tsx
+++ src/app/[locale]/before_and_after/page.tsx
@@ -8,38 +8,39 @@
   import { getImages } from '@/app/_components/getImage';
   import { getLocale } from "next-intl/server";
 
-  export const getTranslatedtext = async () =>  {
-    try {
-        const locale = await getLocale();
-        console.log("locale is " + locale);
-        let beforeText = "";
-        let afterText = "";
+//   export const getTranslatedtext = async () =>  {
+//     try {
+//         const locale = await getLocale();
+//         console.log("locale is " + locale);
+//         let beforeText = "";
+//         let afterText = "";
         
-        if (locale == "en") {
-            beforeText = "BEFORE";
-            afterText = "AFTER";
-        } else {
-            beforeText = "AVANT";
-            afterText = "APRÈS";
-        }
-        return (
-            {
-                before: beforeText,
-                after: afterText,
-            });
-    } catch (error: any) {
-        console.log(error);
-    }
-}
+//         if (locale == "en") {
+//             beforeText = "BEFORE";
+//             afterText = "AFTER";
+//         } else {
+//             beforeText = "AVANT";
+//             afterText = "APRÈS";
+//         }
+//         return (
+//             {
+//                 before: beforeText,
+//                 after: afterText,
+//             });
+//     } catch (error: any) {
+//         console.log(error);
+//     }
+// }
 
 export default async function BeforeAfterPage() {
   
     /* Reads the content of the gallery sub dir and returns an array of strings */
     const beforeImageFilenames =  await getImages("before");
     const afterImageFilenames =  await getImages("after");
-    const words = await getTranslatedtext();
-    const before = words?.before!;
-    const after = words?.after!;
+    // const words = await getTranslatedtext();
+    const locale = await getLocale();
+    const before = "BEFORE";
+    const after = "AFTER";
     return (
         <>
             <main className="common-page-container">
@@ -48,7 +49,7 @@ export default async function BeforeAfterPage() {
                     <div className="container grid grid-cols-7 m-auto w-full">
                         {/* before */}
                         <div className="ba-header col-span-3 w-full flex items-center justify-center">
-                                {before}
+                                {locale=="EN" ? "BEFORE" : "AVANT"}
                         </div>
                         {/* arrow */}
                         <div className="gallery-container w-full flex items-center justify-center">
@@ -56,7 +57,7 @@ export default async function BeforeAfterPage() {
                         </div>
                         {/* after */}
                         <div className="ba-header col-span-3 w-full flex items-center justify-center">
-                                {after}
+                                {locale=="EN" ? "AFTER" : "APRÈS"}
                         </div>
                     </div>
                     
