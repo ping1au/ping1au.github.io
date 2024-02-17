b3001c304e915e4a703cd4b19f981c6f3f277a40
ping1au <>
2024-02-17 21:48:14 +0800

    updated not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,62 @@
 'use client';
 import Error from 'next/error';
-import Footer from "@/app/_components/footer";
-import Navigation from "@/app/_components/navigation";
-import { NextIntlClientProvider } from 'next-intl';
-import {redirect} from 'next/navigation';
-
+import "@/app/globals.css";
+import Image from 'next/image';
+import Link from 'next/link';
 // export default function NotFound() {
 //     redirect('/en/not-found');
 // }
 
 export const dynamicParams = false;
 
-export default function NotFound() {
+export default function NotFound()   {
     return (
       <html lang="en">
-        <body>
+        {/* <body>
+            We are sorry
           <Error statusCode={404} />
-        </body>
+        </body> */}
+       <body>
+            <div className="common-page-container flex flex-col h-screen items-center" style={{whiteSpace: "pre-wrap"}}>
+                <div className='intro-container '>
+                    <Link href="/" style={{display:"block"}}>
+                        <Image
+                        src="/images/logo.png"
+                        alt="Logo"
+                        className="relative"
+                        width={100}
+                        height={100}            
+                        sizes='100vw'
+                        />
+                    </Link>
+                {/* <Error statusCode={404}>
+                
+                    </Error> */}
+                </div>
+                <div className="title-container mx-auto py-8 text-base md:text-lg lg:text-xl xl:text-2xl">
+                    We're sorry.<br/>Nous sommes navrés.
+                </div>
+                <div className='intro-container'>
+                    The page you requested isn't available.<br/>Can't find what you're looking for? Start from our homepage.
+                </div>
+                <div className='intro-container'>
+                La page que vous avez demandé n’est pas disponible.<br/>Vous ne trouvez pas ce que vous cherchez ? Commencez sur la page d’accueil.
+                </div>
+                <div className='intro-container'>
+                <Link href="/" style={{display:"block"}}>
+                        <Image
+                        src="/images/logo.png"
+                        alt="Logo"
+                        className="relative"
+                        width={100}
+                        height={100}            
+                        sizes='100vw'
+                        />
+                    </Link>
+                </div>
+            </div>
+       </body>
+            
       </html>
     );
   }
